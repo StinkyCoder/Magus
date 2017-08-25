d020839f090e5caf5b6e70d6ed8d0cfe1358bce3
StinkyCoder <per.david.bjorklund@gmail.com>
2017-08-25 14:37:24 +0200

    so it begins

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
