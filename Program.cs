308eddf8687bbf2cfb71a703c23a506803c9d4e0
StinkyCoder <per.david.bjorklund@gmail.com>
2017-08-29 22:08:46 +0200

    i got sprites

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
 
 namespace Magus
 {
@@ -6,7 +9,25 @@ namespace Magus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+#if DEBUG
+            Console.WriteLine("Start Magus!");
+#endif
+
+            MainWindow mainWindow = new MainWindow(new GameWindow(1280, // initial width
+                                                                720, // initial height
+                                                                OpenTK.Graphics.GraphicsMode.Default,
+                                                                "Magus",  // initial title
+                                                                GameWindowFlags.Default,
+                                                                DisplayDevice.Default,
+                                                                4, // OpenGL major version
+                                                                0, // OpenGL minor version
+                                                                OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible));
+            mainWindow.Run(30.0);
+            
+#if DEBUG
+            Console.WriteLine( "Exiting main loop...");
+#endif
         }
     }
 }
---
+++ world.cs
@@ -0,0 +1,27 @@
+using System;
+
+/*
+---------------------------------------
+	The Game-world
+---------------------------------------
+*/
+class World{
+
+private const int WORLD_X_MAX = 200;
+private const int WORLD_Y_MAX = 320;
+private short gGateX, gGateY;
+
+private object sprites;
+ private object  gCharacters = null;
+ private object  gThings = null;
+
+
+        public World(){
+           Initialize(); 
+        }
+
+        private void Initialize(){
+
+        }
+
+}
